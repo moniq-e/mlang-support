29981dc120d66e56097d04bd5cc8bb7d3b5ffd32
Monique <otavioschimith124@gmail.com>
2026-03-19 16:20:32 -0300

    testando

--- extension.js
+++ extension.js
@@ -1,12 +1,19 @@
 const vscode = require('vscode');
+const path = require('path');
 
+/**
+ * @param {vscode.ExtensionContext} context
+ */
 function activate(context) {
     let disposable = vscode.commands.registerCommand('mlang.runFile', function () {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
             const terminal = vscode.window.activeTerminal || vscode.window.createTerminal("Execução");
             terminal.show();
-            terminal.sendText(`mlang.jar "${editor.document.fileName}"`);
+
+            const jarPath = path.join(context.extensionPath, 'mlang.jar');
+            const filePath = editor.document.fileName;
+            terminal.sendText(`java -jar "${jarPath}" "${filePath}`);
         }
     });
 
