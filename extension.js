7012dc3d9bd7bdd2c9e5a35e30f114a63b745d01
Monique <otavioschimith124@gmail.com>
2026-03-20 12:46:36 -0300

    download mlang jar on extension activate

--- extension.js
+++ extension.js
@@ -1,23 +1,48 @@
-const vscode = require('vscode');
-const path = require('path');
+const vscode = require('vscode')
+const path = require('path')
+const fs = require('fs')
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('mlang.runFile', function () {
-        const editor = vscode.window.activeTextEditor;
+    downloadMLangExec(context)
+
+    let disposable = vscode.commands.registerCommand('mlang.runFile', () => {
+        const editor = vscode.window.activeTextEditor
         if (editor) {
-            const terminal = vscode.window.activeTerminal || vscode.window.createTerminal("Execução");
-            terminal.show();
+            const terminal = vscode.window.activeTerminal || vscode.window.createTerminal("Execução")
+            terminal.show()
 
-            const jarPath = path.join(context.extensionPath, 'mlang.jar');
-            const filePath = editor.document.fileName;
-            terminal.sendText(`java -jar "${jarPath}" "${filePath}"`);
+            const jarPath = path.join(context.extensionPath, 'mlang.jar')
+            const filePath = editor.document.fileName
+            terminal.sendText(`java -jar "${jarPath}" "${filePath}"`)
         }
-    });
+    })
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable)
+}
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function downloadMLangExec(context) {
+    try {
+        //await vscode.workspace.fs.createDirectory(context.extensionPath)
+
+        const fileUri = vscode.Uri.joinPath(context.extensionPath, "mlang.jar")
+
+        const response = await fetch("https://jitpack.io/com/github/moniq-e/mlang/v0.1.0/mlang-v0.1.0.jar")
+        const arrayBuffer = await response.arrayBuffer()
+        const data = new Uint8Array(arrayBuffer)
+
+        await vscode.workspace.fs.writeFile(fileUri, data)
+        fs.chmodSync(fileUri.path, 0o755)
+
+        console.log(`Salvo em: ${fileUri.fsPath}`)
+    } catch (err) {
+        vscode.window.showErrorMessage(`Erro ao salvar binário: ${err.message}`)
+    }
 }
 
 function deactivate() {}
